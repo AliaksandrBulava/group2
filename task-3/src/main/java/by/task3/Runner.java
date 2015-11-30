587343d716a4219d88e480b0739c47e8ad590af5
Kiryl Parfiankou <kiryl_parfiankou@epam.com>
2015-12-01 01:08:21 +0300

    Task 3: The initial state was added.

--- task-3/src/main/java/by/task3/Runner.java
+++ task-3/src/main/java/by/task3/Runner.java
@@ -1,10 +1,17 @@
 package by.task3;
 
+import by.task3.figures.Circle;
+import by.task3.figures.Rectangle;
+import by.task3.figures.Square;
+
+import javax.annotation.Resource;
+
 /**
- * Created by Julia on 01.12.2015.
+ * Created by Kiryl_Parfiankou on 11/30/2015.
  */
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Task 3");
+
     }
 }
