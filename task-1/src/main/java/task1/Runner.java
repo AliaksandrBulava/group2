6d52b041e7c4ec1d0b07fdf78e78bcaf3ecee586
Kiryl Parfiankou <Kiryl_Parfiankou@epam.com>
2015-12-14 16:34:09 +0300

    Task 1: Runner Initialisation.

---
+++ task-1/src/main/java/task1/Runner.java
@@ -0,0 +1,10 @@
+package task1;
+
+/**
+ * Created by Kiryl_Parfiankou on 12/14/2015.
+ */
+public class Runner {
+    public static void main(String [] args) {
+        System.out.println("Task 1.");
+    }
+}
