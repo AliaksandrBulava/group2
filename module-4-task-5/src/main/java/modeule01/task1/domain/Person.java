bd03e677f6fbbf82b430966b92dc7d5f81edca71
Kiryl Parfiankou <Kiryl_Parfiankou@epam.com>
2016-02-01 17:50:42 +0300

    Module-4 Task 5: Finish task.

---
+++ module-4-task-5/src/main/java/modeule01/task1/domain/Person.java
@@ -0,0 +1,46 @@
+package modeule01.task1.domain;
+
+/**
+ * Created by Kiryl_Parfiankou on 12/14/2015.
+ */
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return name + ',' + age + ',' + gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ module-4-task-5/src/main/java/modeule01/task1/factory/PersonAbstractFactory.java
@@ -0,0 +1,61 @@
+package modeule01.task1.factory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import modeule01.task1.dao.PersonDao;
+import modeule01.task1.dao.db.DbPersonDao;
+import modeule01.task1.dao.file.FilePersonDao;
+
+/**
+ * Created by Kiryl_Parfiankou on 12/14/2015.
+ */
+public class PersonAbstractFactory {
+
+    private final static String CONFIG_FILE = "config.properties";
+
+    private final static String DB_DAO ="db";
+    private final static String FILE_DAO ="file";
+
+    private final static String DAO_PROPERTY ="dao";
+    private final static String FILE_NAME_PROPERTY = "file.name";
+
+    private static PersonDao instance;
+
+    private PersonAbstractFactory() {
+
+    }
+
+    public static synchronized PersonDao getInstance() {
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        if (instance == null) {
+
+            input = PersonAbstractFactory.class.getClassLoader().getResourceAsStream(CONFIG_FILE);
+            if(input==null){
+                System.out.println("Sorry, unable to find " + CONFIG_FILE);
+                return null;
+            }
+            try {
+                prop.load(input);
+                String daoType = prop.getProperty(DAO_PROPERTY);
+                String fileName = prop.getProperty(FILE_NAME_PROPERTY);
+
+                if (DB_DAO.equals(daoType)) {
+                    instance = new DbPersonDao();
+                } else if(FILE_DAO.equals(daoType)) {
+                    instance = new FilePersonDao(fileName);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return instance;
+    }
+
+}
