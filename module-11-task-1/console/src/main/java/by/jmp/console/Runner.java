b459e3dfea5afac420e411345bbc15a099947d83
Kiryl Parfiankou <kiryl_parfiankou@epam.com>
2016-03-01 00:56:13 +0300

    Module 11: Gradle and Maven Task. Initialisation commit.

---
+++ module-11-task-1/console/src/main/java/by/jmp/console/Runner.java
@@ -0,0 +1,10 @@
+package by.jmp.console;
+
+/**
+ * Created by Julia on 01.03.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Gradle and Maven Task");
+    }
+}
