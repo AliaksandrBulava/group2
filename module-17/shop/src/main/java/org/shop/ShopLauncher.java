db8c20f4bcb19ddb80e13263b5ff9e414d3c3fca
Kiryl Parfiankou <Kiryl_Parfiankou@epam.com>
2016-04-05 00:35:45 +0100

    Module-17: Finish module tasks.

--- module-17/shop/src/main/java/org/shop/ShopLauncher.java
+++ module-17/shop/src/main/java/org/shop/ShopLauncher.java
@@ -1,9 +1,13 @@
 package org.shop;
 
 
+import org.shop.api.*;
+import org.shop.data.Item;
+import org.shop.data.Order;
+import org.shop.data.Product;
+import org.shop.data.Proposal;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.applet.AppletContext;
+import org.springframework.context.ApplicationContext;
 
 /**
  * The ShopLauncher class.
@@ -16,6 +20,26 @@ public class ShopLauncher {
      * @param args the arguments
      */
     public static void main(String[] args) {
-        //AppletContext appletContext = new ClassPathXmlApplicationContext("context.xml");
+        ApplicationContext appletContext = new ClassPathXmlApplicationContext("context.xml");
+
+        ProductService productService = appletContext.getBean(ProductService.class);
+        OrderService orderService = appletContext.getBean(OrderService.class, "OrderServiceImpl");
+        ItemService itemService = (ItemService) appletContext.getBean("itemService");
+        UserService userService = (UserService) appletContext.getBean("UserSv");
+        ProposalService proposalService = appletContext.getBean(ProposalService.class);
+
+        Product galaxy = productService.getProductsByName("Samsung Galaxy Tab").get(0);
+        Proposal proposal = proposalService.getProposalsByProduct(galaxy).get(0);
+
+        orderService.createOrder(userService.getUserById((long) 1), proposal);
+
+        for (Order order : orderService.getOrdersByUserId((long) 1)) {
+            System.out.println(order);
+
+            for (Item item : itemService.getItemsByOrderId(order.getId())) {
+                System.out.println(item);
+            }
+        }
+
     }
 }
